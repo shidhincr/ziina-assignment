a115fe67b59ddd0d193dba9c86225ee8a21369d9
Shidhin <shidhincr@gmail.com>
2023-08-19 16:46:09 +0530

    Final working solution

--- src/lib/calculate.ts
+++ src/lib/calculate.ts
@@ -16,68 +16,26 @@ export const getIntersection = (box1: Box, box2: Box): null | Box => {
   }
 }
 
-export const getMutuallyIntersectingBoxes = (boxes: Box[]): Box[] => {
-  let currentBox: Box;
-  let intersectingBoxes: Box[] = []
-  let boxesToIterate = [...boxes]
-  while (boxesToIterate.length) {
-    currentBox = boxesToIterate.shift()
-    boxesToIterate.forEach(box => {
-      intersectingBoxes.push(getIntersection(currentBox, box))
-    });
-  }
-  return intersectingBoxes.filter(Boolean) as Box[];
-}
-
-export const getIntersectingArea = (box1: Box, box2: Box) => {
-  if (!isIntersecting(box1, box2)) return 0
-  const width = Math.min(box1.xx, box2.xx) - Math.max(box1.x, box2.x);
-  const height = Math.min(box1.yy, box2.yy) - Math.max(box1.y, box2.y);
-  return width * height;
-}
-
 export const calculateArea = (box: Box) => {
   return (box.xx - box.x) * (box.yy - box.y);
 }
 
-export const deduplicate = (boxes: Box[]): Box[] => {
-  return boxes.filter((box, i) => {
-    return boxes.findIndex(b => b.x === box.x && b.y === box.y && b.xx === box.xx && b.yy === box.yy) === i
-  })
-}
-
 export const calculateVisibleArea = (base: Box, overlays: Box[]): number => {
   const intersectingBoxes = overlays.map(oBox => getIntersection(base, oBox)).filter(Boolean) as Box[]
-  console.log({ intersectingBoxes })
-  let area = calculateArea(base);
-  console.log({ base, area })
-  for (let i = base.x; i <= base.xx; i++) {
-    for (let j = base.y; j <= base.yy; j++) {
-      for (let b of intersectingBoxes) {
-        // console.log({ i, j, b })
-        if (i >= b.x && i <= b.xx && j >= b.y && j <= b.yy) area--;
+  let baseArea = calculateArea(base)
+  let overlayArea = 0;
+  let overlayMap = {}
+
+  for (let b of intersectingBoxes) {
+    let { x, y, xx, yy } = b
+    for (let i = x; i < xx; i++) {
+      for (let j = y; j < yy; j++) {
+        if (!overlayMap[`${i},${j}`]) {
+          overlayArea++;
+        }
+        overlayMap[`${i},${j}`] = true
       }
     }
   }
-  console.log({ areaCalculated: area })
-  return area;
-}
-// ----------------------
-export const __calculateVisibleArea = (base: Box, overlays: Box[]): number => {
-  const baseArea = calculateArea(base)
-  const sum = (arr: number[]) => arr.reduce((acc, cur) => acc + cur, 0);
-
-  const overlappingBoxes = overlays.map(oBox => {
-    return getIntersection(base, oBox)
-  }).filter(Boolean) as Box[]
-
-  const totalOverlappingArea = sum(overlappingBoxes.map(calculateArea))
-  const intersectingBoxes = getMutuallyIntersectingBoxes(overlappingBoxes);
-  const intersectingBoxesArea = sum(intersectingBoxes.map(calculateArea))
-
-  console.log({ intersectingBoxes })
-
-  const intersectingMutualAreaBoxes = deduplicate(getMutuallyIntersectingBoxes(intersectingBoxes));
-  const intersectingMutualAreaBoxesArea = sum(intersectingMutualAreaBoxes.map(calculateArea));
-  return baseArea - totalOverlappingArea + (intersectingBoxesArea - intersectingMutualAreaBoxesArea);
+  return baseArea - overlayArea;
 }
--- src/test/calculate.test.ts
+++ src/test/calculate.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from 'vitest'
-import { calculateArea, calculateVisibleArea, isIntersecting, getIntersectingArea, getIntersection, getMutuallyIntersectingBoxes } from '../lib/calculate'
+import { calculateVisibleArea, isIntersecting, getIntersection } from '../lib/calculate'
 
 const baseBox = {
   x: 0, y: 0, xx: 100, yy: 100
@@ -9,28 +9,24 @@ const overlayBox2 = { x: 50, y: 40, xx: 150, yy: 150 } // overlapping
 const overlayBox3 = { x: 50, y: 50, xx: 80, yy: 80 } // fully inner
 
 
-/* const dummyIntersectingBoxes = [
+const baseBox1 = {
+  x: 451, xx: 951, y: 226.2265625, yy: 726.2265625
+}
+const overlayBox4 = { x: 575, xx: 675, y: 376, yy: 476 }
+const intersectingBoxes = [
   {
-    x: 0, y: 0, xx: 100, yy: 100
+    "x": 607,
+    "y": 351,
+    "xx": 807,
+    "yy": 551
   },
   {
-    x: 50, y: 50, xx: 150, yy: 150
-  },
+    "x": 657,
+    "y": 401,
+    "xx": 757,
+    "yy": 501
+  }
 ]
-const dummyIntersectingBoxes1 = [
-  {
-    x: 0, y: 0, xx: 100, yy: 100
-  },
-  {
-    x: 10, y: 60, xx: 60, yy: 90
-  },
-  {
-    x: 80, y: 10, xx: 100, yy: 80
-  },
-  {
-    x: 50, y: 50, xx: 150, yy: 150
-  },
-] */
 
 test('is intersecting', () => {
   expect(isIntersecting(baseBox, overlayBox1)).toBe(false)
@@ -47,34 +43,23 @@ test('get intersecting boxes', () => {
   });
 });
 
-test('calculate the visible area', () => {
-  // let area = calculateVisibleArea(baseBox, [overlayBox1])
-  // expect(area).toBe(10000)
-  let area = calculateVisibleArea(baseBox, [overlayBox1, overlayBox2])
-  expect(area).toBe(7000)
+test('calculate visible area for small rectangle', () => {
+  // const base = { x: 0, y: 0, xx: 10, yy: 10 }
+  // const overlay = { x: 5, y: 5, xx: 15, yy: 15 }
+  const base = { x: 0, y: 0, xx: 5, yy: 5 }
+  const overlay = { x: 3, y: 3, xx: 6, yy: 6 }
+  let area = calculateVisibleArea(base, [overlay])
+  expect(area).toBe(21)
 })
 
-// test('get mutual area boxes', () => {
-//   expect(getMutuallyIntersectingBoxes(dummyIntersectingBoxes)).toEqual([{
-//     x: 50, y: 50, xx: 100, yy: 100
-//   }])
-// });
-//
-// test('calculate intersecting area', () => {
-//   const area1 = getIntersectingArea(baseBox, overlayBox1)
-//   const area2 = getIntersectingArea(baseBox, overlayBox2)
-//   expect(area1).toBe(0)
-//   expect(area2).toBe(3000)
-// })
-//
-// test('calculate visible area', () => {
-//   expect(calculateArea(baseBox)).toBe(10000)
-//   expect(calculateArea(overlayBox2)).toBe(11000)
-// });
-//
-// test('calculate visible area', () => {
-//   let area = calculateVisibleArea(baseBox, [overlayBox1])
-//   expect(area).toBe(10000)
-//   area = calculateVisibleArea(baseBox, [overlayBox1, overlayBox2])
-//   expect(area).toBe(7000)
-// })
+test('calculate the visible area', () => {
+  let area = calculateVisibleArea(baseBox, [overlayBox1])
+  expect(area).toBe(10000)
+  area = calculateVisibleArea(baseBox, [overlayBox1, overlayBox2])
+  expect(area).toBe(7000)
+  area = calculateVisibleArea(baseBox1, [overlayBox4])
+  expect(area).toBe(240000)
+  area = calculateVisibleArea(baseBox1, intersectingBoxes)
+  expect(area).toBe(210000)
+})
+
